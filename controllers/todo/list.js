7c972a96d67cfb78f684b8bcd5eeb07d19b2fe29
Amit Kumar <k.amitkumar305@gmail.com>
2024-03-18 11:46:53 +0530

    Implemented aggregation.

--- controllers/todo/list.js
+++ controllers/todo/list.js
@@ -1,21 +1,40 @@
+const mongoose = require('mongoose')
 const TodoModel = require('../../models/todo.models');
 
 async function TodoList(req, res) {
     try {
         const { id, page = 1, limit = 10 } = req.body;
 
-        const todos = await TodoModel.find({
-            userId: id,
-        })
-        .skip((page-1) * limit)
-        .limit(limit)
-        .select('-__v')
+        const todos = await TodoModel.aggregate([
+            {
+                $match: {
+                    userId: new mongoose.Types.ObjectId(id),
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    todoId: '$_id',
+                    text: '$text',
+                    userId: '$userId',
+                    createdAt: '$createdAt',
+                    updatedAt: '$updatedAt',
+                }
+            },
+            {
+                $skip: (page-1)*limit,
+            },
+            {
+                $limit: limit,
+            },
+        ]);
 
         res.status(200).json({
             message: 'Todos fetched successfully',
             data: todos,
         })
     } catch (err) {
+        console.log(err``);
         res.status(500).json({
             message: 'Error while fetching todo'
         })
